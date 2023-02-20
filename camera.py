451ec95633a90ea06d91532c9b15ff44777a5ce1
thoms2901 <thomas.kirschner2901@gmail.com>
2023-02-20 09:42:03 +0100

    comments

--- camera.py
+++ camera.py
@@ -10,8 +10,10 @@ import uuid
 
 import config
 
+# crea un cookie per l'utente loggato
 cookie_secret = base64.b64encode(uuid.uuid4().bytes + uuid.uuid4().bytes)
 
+# processa le immagini con un timestamp
 class CameraImageProcessor():
 	def __init__(self, in_filename, out_filename, width=640, height=480):
 		self.size = (width, height)
@@ -32,6 +34,7 @@ class CameraImageProcessor():
 	def getDaylightIntensity(self, hour):
 		return 0.45 * math.sin(0.25 * hour + 4.5) + 0.5
 
+# classe delle immagini
 class ImageClass():
 	def __init__(self):
 		self.images = [file for file in os.listdir(config.IMAGES_DIR) if file.endswith(".jpeg")]
@@ -42,6 +45,7 @@ class ImageClass():
 		self.counter = (self.counter + 1)%len(self.images)
 		return str(res)
 
+# gestisce la richiesta della camera e il processing delle foto 
 class ImageHandler(tornado.web.RequestHandler):
 	
 	BOUNDARY = '--boundarydonotcross'
@@ -82,11 +86,12 @@ class ImageHandler(tornado.web.RequestHandler):
 			'Content-Type': 'image/jpeg',
 		}
 
-
+# gestisce la pagina della presa shelly
 class ShellyHandler(tornado.web.RequestHandler):
 	def get(self):
 		return self.render("templates/shelly.html")
 
+# gestisce la pagina contenente la camera
 class HomeHandler(tornado.web.RequestHandler):
 	settings = {
 		'title': config.CAMERA_NAME,
@@ -101,6 +106,7 @@ class HomeHandler(tornado.web.RequestHandler):
 
 
 
+# gestisce la pagina / con il login
 class RootHandler(tornado.web.RequestHandler):
 
 	def get(self):
@@ -119,14 +125,16 @@ class RootHandler(tornado.web.RequestHandler):
 			self.write("Login failed")
 			self.redirect("/")
 
+# inizializza la classe delle immagini
 image = ImageClass()
 
+# modifica gli header del server
 class ServerHeaderTransform(tornado.web.OutputTransform):
     def transform_first_chunk(self, status_code, headers, chunk, finishing):
         headers['Server'] = config.SERVER_HEADER_NAME
         return status_code, headers, chunk
 
-
+# path dei servizi 
 application = tornado.web.Application([
 	(r'/camera', ImageHandler),
 	(r'/home', HomeHandler),
@@ -136,6 +144,7 @@ application = tornado.web.Application([
 	(r'/static/(.*)', tornado.web.StaticFileHandler, {'path': 'static/'}),
 ], transforms=[ServerHeaderTransform], cookie_secret=cookie_secret)
 
+# main
 if __name__ == "__main__":
 	application.listen(config.PORT)
 	tornado.ioloop.IOLoop.instance().start()
